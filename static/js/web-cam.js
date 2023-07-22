d4753f85212a00243402f7985bb9151c13a4212a
Mark Stouffer <1802850+InTEGr8or@users.noreply.github.com>
2023-07-22 10:26:19 -0700

    Add environment facing demo

--- static/js/web-cam.js
+++ static/js/web-cam.js
@@ -16,13 +16,9 @@ const constraints = {
   video: {
     width: {
       min: 1280,
-      ideal: 1920,
-      max: 2560,
     },
     height: {
       min: 720,
-      ideal: 1080,
-      max: 1440
     },
     facingMode: 'environment'
   }
