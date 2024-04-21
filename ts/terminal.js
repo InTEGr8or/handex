89a150603ae36833cd9f6ce849f4f5e5e86d71ed
InTEGr8or <InTEGr8or@users.noreply.github.com>
2024-04-21 00:46:59 +0000

    deploy: a20b8678dcddd6033655b60e1964ac45b84c01ae

--- ts/terminal.js
+++ ts/terminal.js
@@ -32,19 +32,55 @@
     }
   };
   var _TerminalGame = class _TerminalGame {
-    constructor(terminalElement) {
-      this.terminalElement = terminalElement;
+    constructor(terminal) {
+      this.terminal = terminal;
       __publicField(this, "commandHistory", []);
       __publicField(this, "wpmCounter", 0);
       __publicField(this, "startTime", null);
       __publicField(this, "outputElement");
       __publicField(this, "inputElement");
-      this.terminalElement.classList.add("terminal" /* Terminal */);
+      __publicField(this, "lastTouchDistance", null);
+      __publicField(this, "currentFontSize");
+      this.terminal.classList.add("terminal" /* Terminal */);
       this.outputElement = this.createOutputElement();
-      this.terminalElement.appendChild(this.outputElement);
-      this.terminalElement.appendChild(this.createPromptElement());
+      this.terminal.appendChild(this.outputElement);
+      this.terminal.appendChild(this.createPromptElement());
       this.loadCommandHistory();
       this.bindInput();
+      this.addTouchListeners();
+    }
+    addTouchListeners() {
+      this.terminal.addEventListener("touchstart", this.handleTouchStart.bind(this), { passive: false });
+      this.terminal.addEventListener("touchmove", this.handleTouchMove.bind(this), { passive: false });
+      this.terminal.addEventListener("touchend", this.handleTouchEnd.bind(this));
+    }
+    handleTouchStart(event) {
+      if (event.touches.length === 2) {
+        event.preventDefault();
+        this.lastTouchDistance = this.getDistanceBetweenTouches(event.touches);
+      }
+    }
+    handleTouchMove(event) {
+      if (event.touches.length === 2) {
+        event.preventDefault();
+        const currentDistance = this.getDistanceBetweenTouches(event.touches);
+        if (this.lastTouchDistance) {
+          const scaleFactor = currentDistance / this.lastTouchDistance;
+          this.currentFontSize *= scaleFactor;
+          this.terminal.style.fontSize = `${this.currentFontSize}px`;
+          this.lastTouchDistance = currentDistance;
+        }
+      }
+    }
+    handleTouchEnd(event) {
+      this.lastTouchDistance = null;
+    }
+    getDistanceBetweenTouches(touches) {
+      const touch1 = touches[0];
+      const touch2 = touches[1];
+      return Math.sqrt(
+        Math.pow(touch2.pageX - touch1.pageX, 2) + Math.pow(touch2.pageY - touch1.pageY, 2)
+      );
     }
     saveCommandHistory() {
       const historyToSave = this.commandHistory.slice(-_TerminalGame.commandHistoryLimit);
