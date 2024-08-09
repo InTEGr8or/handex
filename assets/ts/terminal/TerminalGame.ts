dfbb05d1c2deb3d004e63f2152e560c4303aa209
Mark Stouffer <1802850+InTEGr8or@users.noreply.github.com>
2024-08-08 17:50:06 -0700

    Remove vetigial handterm code

--- assets/ts/terminal/TerminalGame.ts
+++
@@ -1,9 +0,0 @@
-// TerminalGame.ts
-import { IPersistence } from './Persistence';
-import { IHandexTerm } from './HandexTerm';
-
-export class TerminalGame {
-
-
-    // Additional methods for calculating WPM, updating the progress bar, etc.
-}
